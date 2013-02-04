3f428c72d33e8fa620d2673e0c0c62fead0cbae3
Zack Michener <zack@zackmichener.com>
2013-02-04 10:48:19 -0500

    change makeDate test to use should

--- test/test.js
+++ test/test.js
@@ -1,21 +1,12 @@
 var assert = require("assert")
 var Tracker = require("../Tracker");
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
-
 describe("Tracker", function() {
     describe("#makeDate()", function() {
         it('should make a valid datestamp given a tweet', function() {
             var tracker = new Tracker();
             var sampleTweet = {"created_at": "Thu Oct 21 16:02:46 +0000 2010"};
-            assert.equal("2010-10-21", tracker.makeDate(sampleTweet));
+            tracker.makeDate(sampleTweet).should.equal("2010-10-21");
         });
     });
 });
