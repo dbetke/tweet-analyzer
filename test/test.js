d786696bc68912d7f81ea442ddb2138a1495ce64
Zack Michener <zack@zackmichener.com>
2013-02-05 15:59:19 -0500

    write redis test for tracker

--- test/test.js
+++ test/test.js
@@ -1,12 +1,29 @@
 var assert = require("assert")
-var Tracker = require("../Tracker");
+  , Tracker = require("../tracker")
+  , redis = require("redis")
+  , should = require("should");
 
 describe("Tracker", function() {
-    describe("#makeDate()", function() {
-        it('should make a valid datestamp given a tweet', function() {
+    describe("#track()", function() {
+        it('should add counts to redis when tracking Bieber and Jesus', 
+                                                             function(done) {
             var tracker = new Tracker();
-            var sampleTweet = {"created_at": "Thu Oct 21 16:02:46 +0000 2010"};
-            tracker.makeDate(sampleTweet).should.equal("2010-10-21");
-        });
+
+            this.timeout(15000);
+            tracker.track(["bieber", "jesus"], ["love", "hate"]);
+            setTimeout(function() {
+                var redisClient = redis.createClient()
+                  , date = ("" + new Date().getFullYear() + "-" 
+                               + (new Date().getMonth()+1) + "-" 
+                               + new Date().getDate());
+
+                redisClient.hgetall(date, function(err, result) {
+                    if (err) throw err;
+                    should.exist(result);
+                    done();
+                });
+            }, 5000);
+       }); 
     });
 });
+
