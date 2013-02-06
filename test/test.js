f1867f2b066e3b1c2c7feed42a295982faec9636
Zack Michener <zack@zackmichener.com>
2013-02-06 10:48:38 -0500

    fix test to fail when it should
    flush redis before tracker starts

--- test/test.js
+++ test/test.js
@@ -7,23 +7,26 @@ describe("Tracker", function() {
     describe("#track()", function() {
         it('should add counts to redis when tracking Bieber and Jesus', 
                                                              function(done) {
-            var tracker = new Tracker();
+            var tracker = new Tracker()
+              , redisClient = redis.createClient();
 
             this.timeout(15000);
+            redisClient.flushall();
             tracker.track(["bieber", "jesus"], ["love", "hate"]);
             setTimeout(function() {
-                var redisClient = redis.createClient()
-                  , date = ("" + new Date().getFullYear() + "-" 
-                               + (new Date().getMonth()+1) + "-" 
-                               + new Date().getDate());
+                var date = ("" + new Date().getFullYear() + "-" 
+                               + (new Date().getMonth() + 1) + "-" 
+                               + new Date().getDate() );
 
                 redisClient.hgetall(date, function(err, result) {
                     if (err) throw err;
+                    console.log(result);
                     should.exist(result);
                     done();
                 });
             }, 5000);
        }); 
     });
+
 });
 
