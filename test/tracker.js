489c6b549b3af4330c724e1310d7536a348b29fd
dbetke <danielle.betke@gmail.com>
2013-03-06 11:30:15 -0500

    adds usePrefix function to tracker, changes tracker test to use prefix and to check and remove only tweets with the test prefix, updates to use best practices

--- test/tracker.js
+++ test/tracker.js
@@ -1,105 +1,123 @@
+'use strict';
 process.env.NODE_ENV = 'test';
 
-var assert = require("assert")
-  , redis = require("redis")
-  , should = require("should")
-  , mongodb = require("mongodb")
-  , mongoclient = require("mongodb").Client
-  , twitter = require('immortal-ntwitter')
-  , credentials = require('../credentials.js')
-  , Tracker = require('../tracker');
-  ;
+var assert = require("assert"),
+    redis = require("redis"),
+    should = require("should"),
+    mongodb = require("mongodb"),
+    mongoclient = require("mongodb").Client,
+    twitter = require('immortal-ntwitter'),
+    credentials = require('../credentials.js'),
+    Tracker = require('../tracker');
 
-describe("Tracker", function() {
-var tracker = new Tracker();
-var server = new mongodb.Server("127.0.0.1", 27017, {});
-var db = 'testTweetDb';
-var collection = 'testTweets';
-var redisClient;
+describe("Tracker", function () {
+    var tracker = new Tracker(),
+        server = new mongodb.Server("127.0.0.1", 27017, {}),
+        db = 'testTweetDb',
+        collection = 'testTweets',
+        redisClient,
+        prefix,
+        mongoClient;
 
-    	it('should connect to redis', function(done){
-	    redisClient = redis.createClient();
-	    should.exist(redisClient);
-	    done();
-	});
+    it('should connect to redis', function (done) {
+        redisClient = redis.createClient();
+        should.exist(redisClient);
+        done();
+    });
 
-    describe("#makeDate()", function(){
-	function makeDate(tweet){
-	    var d = (tweet.created_at); 
-	    var month = new Date(Date.parse(d)).getMonth()+1;
-            var day = new Date(Date.parse(d)).getDate();
-            var year = new Date(Date.parse(d)).getFullYear();
-            var date = (year + "-" + month + "-" + day);
-         
-	    return date;
-	}
-	
-	it('should return a reformatted date', function(){
-	    var testTweet = {"created_at": "Wed Feb 13 13:47:20 +0000 2013"};
+    describe("#makeDate()", function () {
+        function makeDate(tweet) {
+            var d = (tweet.created_at),
+                month = new Date(Date.parse(d)).getMonth() + 1,
+                day = new Date(Date.parse(d)).getDate(),
+                year = new Date(Date.parse(d)).getFullYear(),
+                date = (year + "-" + month + "-" + day);
+
+            return date;
+        }
+
+        it('should return a reformatted date', function () {
+            var testTweet = {"created_at": "Wed Feb 13 13:47:20 +0000 2013"};
             makeDate(testTweet).should.equal('2013-2-13');
-	});
-	
+        });
+
     });//end describe makeDate
 
-    describe("#UseCollection()", function(){
-	it('should connect to mongodb', function(done){
-	    tracker.UseCollection(db, collection);
-	    //TODO: Add test to verify collection was created properly (how to connect to database after using method to create?)
-	    done();
-	});
+    describe("#UseCollection()", function () {
+        it('should connect to mongodb', function (done) {
+            tracker.UseCollection(db, collection);
+            //TODO: Add test to verify collection was created properly (how to connect to database after using method to create?)
+            done();
+        });
     }); //end describe UseCollection
 
-    describe("#track()", function(){
-	var subject = ['a', 'i'];
-	var keyword = ['love', 'hate'];
-	var exists;
+    describe("usePrefix()", function () {
+        it('should set the prefix to be used in Redis', function () {
+            prefix = tracker.usePrefix('MochaTest-');
+            prefix.should.equal('MochaTest-');
+        });
+    });
 
-		
-	it('should add counts to redis when tracking', function(done) {                                                                                                       
+    describe("#track()", function () {
+        var subject = ['a', 'i'],
+            keyword = ['love', 'hate'],
+            exists,
+            date;
+
+        it('should add counts to redis when tracking', function (done) {
             this.timeout(25000);
-            redisClient.flushall();
             tracker.track(subject, keyword);
-            setTimeout(function() {
-                var date = ("" + new Date().getFullYear() + "-" 
-                               + (new Date().getMonth() + 1) + "-" 
-                               + new Date().getDate() );
-
-                redisClient.hgetall(date, function(err, result) {
-                    if (err) throw err;
-                    should.exist(result);
-                    done();
+            setTimeout(function () {
+                date = (new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDate());
+                //return all the values associated with the key and fields
+                redisClient.hmget(date, prefix + subject[0] + keyword[0], prefix + subject[0] + keyword[1], prefix + subject[1] + keyword[0], prefix + subject[1] + keyword[1], function (err, result) {
+                    if (err) {
+                        throw err;
+                    }
+                    //add results for test - testing for result should not equal [null, null, null, null] does not work, test passes regardless
+                    result = result[0] + result[1] + result[2] + result[3];
+                    result.should.not.equal(0);
                 });
-            }, 5000);                              
-	});
+                done();
+            }, 5000);
+        });
 
-	it('should add tweets to mongodb', function(done){
-	    //TODO: add test to verify tracked tweets are being saved to mongo
-	    done();
-	});
+        it('should add tweets to mongodb', function (done) {
+            //TODO: add test to verify tracked tweets are being saved to mongo
+            done();
+        });
 
+        it('should use regular expressions to match keywords to tweets', function (done) {
+            keyword = ["love", "hate"];
+            var testTweet1 = 'i love this test',
+                testTweet2 = 'i hate this test',
+                testTweet3 = "i don't care about this test",
+                keyword1_re = new RegExp("(\\s|^)" + keyword[0] + "(\\s|$)", "i"),
+                keyword2_re = new RegExp("(\\s|^)" + keyword[1] + "(\\s|$)", "i");
 
-	it('should use regular expressions to match keywords to tweets', function(done){
-	    keyword = ["love", "hate"]
-	    var testTweet1 = 'i love this test';
-	    var testTweet2 = 'i hate this test';
-	    var testTweet3 = 'i am indifferent about this test';
-	    var keyword1_re = new RegExp("(\\s|^)" + keyword[0] + "(\\s|$)", "i");
-            var keyword2_re = new RegExp("(\\s|^)" + keyword[1] + "(\\s|$)", "i");
+            //verify that the appropriate matches are made using the regex
+            if ((testTweet1.match(keyword1_re)) && (testTweet2.match(keyword2_re)) && (!testTweet3.match(keyword1_re)) && (!testTweet3.match(keyword2_re))) {
+                done();
+            } else {
+                throw 'uh oh: matching problem!';
+            }
 
-	    //verify that the appropriate matches are made using the regex
-	    if((testTweet1.match(keyword1_re)) && (testTweet2.match(keyword2_re)) && (!testTweet3.match(keyword1_re)) && (!testTweet3.match(keyword2_re))){
-		done();
-	    }
-	    else{
-		throw 'uh oh: matching problem!';
-	    }
+            //TODO: Look into why the following convention will not work (times out)
+            //testTweet1.should.match(keyword1_re);
+            //testTweet2.should.match(keyword2_re);
+        });
 
-	    //TODO: Look into why the following convention will not work (times out)
-	    //testTweet1.should.match(keyword1_re);
-	    //testTweet2.should.match(keyword2_re);
-	});
+        //CLEAR DATABASE AFTER TESTS ARE COMPLETE
+        after(function (done) {
+            redisClient.hdel(date, prefix + subject[0] + keyword[0]);
+            redisClient.hdel(date, prefix + subject[0] + keyword[1]);
+            redisClient.hdel(date, prefix + subject[1] + keyword[0]);
+            redisClient.hdel(date, prefix + subject[1] + keyword[1]);
+
+            //TODO: add code to drop database and close connection
+            done();
+        });
 
     });//end describe track
-
 }); //end describe tracker
 
--- tracker.js
+++ tracker.js
@@ -1,112 +1,105 @@
-var twitter = require('immortal-ntwitter'),
+'use strict';
+
+var Twitter = require('immortal-ntwitter'),
     redis = require('redis'),
     cf = require('./cloudfoundry'),
     credentials = require('./credentials.js'),
     mongodb = require('mongodb'),
     mongoclient = require('mongodb').Client;
 
-var collection; //mongo database collection
-var server = new mongodb.Server("127.0.0.1", 27017, {});
-
 function Tracker() {
-    var client;
-    var redis_host =  cf.redis?cf.redis.credentials.host:'localhost';
-    var redis_port = cf.redis?cf.redis.credentials.port:6379;
-    var redis_password = cf.redis?cf.redis.credentials.password:undefined;
+    var client,
+        collection, //mongo database collection
+        prefix,
+        server = new mongodb.Server("127.0.0.1", 27017, {}),
+        redis_host =  cf.redis ? cf.redis.credentials.host : 'localhost',
+        redis_port = cf.redis ? cf.redis.credentials.port : 6379,
+        redis_password = cf.redis ? cf.redis.credentials.password : undefined,
+        t = new Twitter({
+            consumer_key: credentials.consumer_key,
+            consumer_secret: credentials.consumer_secret,
+            access_token_key: credentials.access_token_key,
+            access_token_secret: credentials.access_token_secret
+        }),
+        makeDate = function (tweet) {
+            var d = (tweet.created_at),
+                month = new Date(Date.parse(d)).getMonth() + 1,
+                day = new Date(Date.parse(d)).getDate(),
+                year = new Date(Date.parse(d)).getFullYear(),
+                date = (year + "-" + month + "-" + day);
+
+            return date;
+        };
 
     client = redis.createClient(redis_port, redis_host);
-    
-    if(cf.runningInTheCloud) {
+
+    if (cf.runningInTheCloud) {
         client.auth(redis_password);
     }
 
-    var t = new twitter({
-        consumer_key: credentials.consumer_key,
-        consumer_secret: credentials.consumer_secret,
-        access_token_key: credentials.access_token_key,
-        access_token_secret: credentials.access_token_secret
-    });
-
-    var makeDate = function(tweet) {
-        var d = (tweet.created_at);
-        var month = new Date(Date.parse(d)).getMonth()+1;
-        var day = new Date(Date.parse(d)).getDate();
-        var year = new Date(Date.parse(d)).getFullYear();
-        var date = (year + "-" + month + "-" + day);
-
-        return date;
+    this.UseCollection = function (dbName, collectionName) {
+        new mongodb.Db(dbName, server, {w: 1}).open(function (error, client) {
+            if (error) {
+                console.log(error);
+            } else {
+                collection = new mongodb.Collection(client, collectionName);
+                console.log('mongodb is connected!');
+            }
+        });
     };
 
-    this.UseCollection = function(dbName, collectionName){
-	new mongodb.Db(dbName, server, {w:1}).open(function (error, client) {
-	    if (error){
-		console.log(error);
-	    }
-	    else{
-		collection = new mongodb.Collection(client, collectionName);
-		console.log('mongodb is connected!');
-	    }
-	});
-    }
+    this.usePrefix = function (pre) {
+        prefix = pre;
+        return prefix;
+    };
 
-    this.track = function(subjects, keywords) {
+    this.track = function (subjects, keywords) {
         t.immortalStream(
             'statuses/filter',
             { track: subjects },
-            function(stream) {
-                stream.on('data', function(tweet) {
+            function (stream) {
+                stream.on('data', function (tweet) {
                     if (tweet.text === undefined) {
                         // data received is not actually a tweet
                         return;
                     }
 
-                    var date = makeDate(tweet);
-                    var tweetString = JSON.stringify(tweet); //convert object to string for storage
-                    var keyword1_re = new RegExp("(\\s|^)" + keywords[0] + "(\\s|$)", "i"); 
-                    var keyword2_re = new RegExp("(\\s|^)" + keywords[1] + "(\\s|$)", "i"); 
+                    var date = makeDate(tweet),
+                        tweetString = JSON.stringify(tweet), //convert object to string for storage
+                        keyword1_re = new RegExp("(\\s|^)" + keywords[0] + "(\\s|$)", "i"),
+                        keyword2_re = new RegExp("(\\s|^)" + keywords[1] + "(\\s|$)", "i");
 
-                    subjects.forEach(function(subject) {    
-                        if(tweet.text.match(subject)) {
-                            if(tweet.text.match(keyword1_re)) {
+                    subjects.forEach(function (subject) {
+                        if (tweet.text.match(subject)) {
+                            if (tweet.text.match(keyword1_re)) {
                                 //increment count in redis db
-                                client.hincrby(date, subject+keywords[0],'1', redis.print);
+                                client.hincrby(date, prefix + subject + keywords[0], '1', redis.print);
                                 //write to the console (for testing)
                                 //console.log(subject + " " + keywords[0] + "\nTweet: " + tweet.text);
                                 //add to the database
-                                collection.insert({subject: subject, 
-                                                   keyword: keywords[0], 
-                                                   date: date, 
-                                                   tweet: tweetString}, 
-                                                  {safe:true}, 
-                                        function(err, objects) {
+                                collection.insert({subject : subject, keyword : keywords[0], date : date, tweet : tweetString}, {safe : true}, function (err, objects) {
                                     if (err) {
                                         console.log(err);
-                                    } else {
-                                        //write to the console (for testing)
-                                        //console.log("the tweet was saved to the database\n" + tweetString);
                                     }
                                 });
-                            };
+                            }
 
-                            if(tweet.text.match(keyword2_re)) {
+                            if (tweet.text.match(keyword2_re)) {
                                 //increment count in redis db
-                                client.hincrby(date, subject+keywords[1], '1', redis.print);
+                                client.hincrby(date, prefix + subject + keywords[1], '1', redis.print);
                                 //write to the console (for testing)
                                 //console.log(subject + " " + keywords[1] + "\nTweet: " + tweet.text);
                                 //add to the database
-                                collection.insert({subject: subject, keyword: keywords[1], date: date, tweet: tweetString}, {safe:true}, function(err, objects) {
-                                    if (err){
+                                collection.insert({subject : subject, keyword : keywords[1], date : date, tweet : tweetString}, {safe : true}, function (err, objects) {
+                                    if (err) {
                                         console.log(err);
-                                    } else {
-                                        //write to the console (for testing)
-                                        //console.log("the tweet was saved to the database\n" + tweetString);
                                     }
                                 });
                             }
                         } // if(tweet.text.match(subject))
                     }); // subjects.forEach
                 }); // stream.on
-                
+
                 stream.on('error', function (err) {
                     console.log(err);
                 });
